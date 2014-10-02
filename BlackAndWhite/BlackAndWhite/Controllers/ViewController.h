90059655998c9a261a36b5604f1c6665842e6a55
Вячеслав Ембатуров <portweinson@gmail.com>
2014-10-02 18:02:42 +0400

    initial project files

---
+++ BlackAndWhite/BlackAndWhite/Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlackAndWhite
+//
+//  Created by Вячеслав Ембатуров on 02.10.14.
+//  Copyright (c) 2014 Вячеслав Ембатуров. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
